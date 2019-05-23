866757a3c14db968042307eae11494afb26208e9
John.Skibicki <john.skibicki@chifcb-nmx7171.na.corp.ipgnetwork.com>
2019-05-23 12:55:16 -0500

    Working alright

--- WaveLoader/WaveLoader/__init__.py
+++ WaveLoader/WaveLoader/__init__.py
@@ -1,12 +1,14 @@
 import wave
 import torch
 import struct
+import numpy as np
 
 
 class WaveLoader:
     def __init__(self, wave_file_name):
         self.wave_file_name = wave_file_name
         self.wr = wave.open(self.wave_file_name, 'rb')
+        print(self.wr.getparams())
 
     def __del__(self):
         try:
@@ -19,13 +21,37 @@ class WaveLoader:
             self.wr.rewind()
         sample_rate = self.wr.getframerate()
         frame = self.wr.readframes(sample_rate)
-        frame_iter = struct.iter_unpack('<h', frame)
-        _data = [x[0] for x in frame_iter]
+        if len(frame) < sample_rate:  # if we get a part sample just loop back over again
+            self.wr.rewind()
+            frame = self.wr.readframes(sample_rate)
+        fmt = '<%dh' % sample_rate
+        frame_iter = struct.unpack(fmt, frame)
+        _data = list(frame_iter)
         data = torch.tensor(_data)
         if self.wr.tell() == self.wr.getnframes():
             self.wr.rewind()
         frame = self.wr.readframes(sample_rate)
-        frame_iter = struct.iter_unpack('<h', frame)
-        _data = [x[0] for x in frame_iter]
+        if len(frame) < sample_rate:
+            self.wr.rewind()
+            frame = self.wr.readframes(sample_rate)
+        frame_iter = struct.unpack(fmt, frame)
+        _data = list(frame_iter)
         label = torch.tensor(_data)
         return data, label
+
+    def write_wave(self, tensors, file_name, nchannels, sampwidth, framerate):
+        _list = tensors.tolist()
+        ww = wave.open(file_name, 'wb')
+        ww.setnchannels(nchannels)
+        ww.setsampwidth(sampwidth)
+        ww.setframerate(framerate)
+        fmt = '<%dh' % framerate
+        barr = bytearray()
+        for sample in _list:
+            slist = np.short(sample).tolist()
+            blist = struct.pack(fmt, *slist)
+            barr += blist
+
+        ww.writeframesraw(barr)
+
+
--- WaveLoader/test.py
+++ WaveLoader/test.py
@@ -1,7 +1,25 @@
 from WaveLoader import WaveLoader
+import torch
 
-sample = './pemex000.wav'
+sample = './pemex8k.wav'
 wl = WaveLoader(sample)  # type WaveLoader
 data, label = wl.get_sample()
 
 print(data.shape, label.shape)
+
+# Write junk
+t = torch.rand((5, 8000))
+t = torch.mul(t, 65535)
+t = torch.sub(t, 65535/2)
+
+wl.write_wave(t, './output.wav', 1, 2, 8000)
+
+
+#write pemex
+buff = [data.tolist()]
+for x in range(5):
+    data, label = wl.get_sample()
+    buff.append(data.tolist())
+
+pemex = torch.tensor(buff)
+wl.write_wave(pemex, './output-pemex.wav', 1, 2, 8000)
